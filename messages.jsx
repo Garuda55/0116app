78d110086e93174571c97c2dedb83279aff72395
Garuda55 <rekon55@mail.ru>
2024-06-21 14:00:05 +0300

    Create messages.jsx

---
+++ messages.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export const Messages = () => {
+  const [advice, setAdvice] = useState("");
+  
+  useEffect(() => {
+    const url = "https://api.adviceslip.com/advice";
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+
+        console.log(json.slip.advice);
+        setAdvice(json.slip.advice);
+      } catch (error) {
+        console.log("Error ", error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className="container">
+      <p className="text-center text-2xl text-green-600">Случайный совет: <span className="text-red-500">{advice}</span></p>
+    </div>
+  );
+};
